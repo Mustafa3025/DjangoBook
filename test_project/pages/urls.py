d320ee400afcd0c0a4e2097147575c3cbcf61aa7
Mustafa <s.mmustafa3025@gmail.com>
2026-08-16 12:41:59 +0500

    initial commit

--- test_project/pages/urls.py
+++ test_project/pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, AboutPageView
 
-urlpatterns = {
-    path('', homePageView, name = 'home')
-}
+urlpatterns = [
+    #path('', homePageView, name = 'home') non class based view
+    #class vased view below
+    path('about/', AboutPageView.as_view(), name = 'about'),
+    path('', HomePageView.as_view(), name = 'home')
+]
